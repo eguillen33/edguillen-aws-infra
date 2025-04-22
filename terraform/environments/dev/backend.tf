f6237f20a475abb483fc7d29ba0006522bc9cbe7
Ed Guillen <id4eguillen@gmail.com>
2025-04-22 13:21:22 -0700

    Format terraform files

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,16 +1,16 @@
 terraform {
-    backend "s3" {
-        bucket          = "edguillen-terraform-state"
-        key             = "eks/dev/terraform.tfstate"
-        region          = "us-west-2"
-        encrypt         = true
-        use_lockfile    = true
-    }
+  backend "s3" {
+    bucket       = "edguillen-terraform-state"
+    key          = "eks/dev/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -19,8 +19,8 @@ variable "vpc_cidr" {
 }
 
 variable "private_subnets" {
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnets" {
